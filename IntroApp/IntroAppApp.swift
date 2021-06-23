932d4824bc0ce325c1237ca31c471d63258c0618
Jun Hao <neojunhao2008@gmail.com>
2021-06-23 09:59:28 +0800

    Initial Commit

---
+++ IntroApp/IntroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroAppApp.swift
+//  IntroApp
+//
+//  Created by sap on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct IntroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
